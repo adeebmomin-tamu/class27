b2a4b8e97c47f2600bda1a2989b65b0d0a54951a
adeebmomin-tamu <156865104+adeebmomin-tamu@users.noreply.github.com>
2025-03-31 16:23:05 -0500

    Update vuln.cpp

--- vuln.cpp
+++ vuln.cpp
@@ -1,8 +1,9 @@
-#include <cstdlib>
 #include <iostream>
+#include <cstring>
 
 int main(int argc, char* argv[]) {
-    std::cout << "Running: " << argv[1] << std::endl;
-    system(argv[1]); // 🚨 Command injection
+    char buffer[10];
+    strcpy(buffer, argv[1]); // 🚨 Classic buffer overflow
+    std::cout << "You entered: " << buffer << std::endl;
     return 0;
 }
